cf39145dbd14f7de2e8915d568217be73b76a298
mjip <marie.payne@canada.ca>
2018-10-31 11:33:58 -0400

    file stat timing

---
+++ test_file_stat_time.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+import time
+import sys
+import stat
+
+try: fname = sys.argv[1]
+except: 
+	print("Usage: specify file to test file stats on as first arg")
+	sys.exit(0)
+
+start = time.time()
+
+lstat = os.stat(fname)
+mtime = lstat[stat.ST_MTIME]
+
+end = time.time()
+print("It took {0} to check the mtime of the file".format(end-start))
+
+start = time.time()
+
+fsiz = lstat[stat.ST_SIZE]
+
+end = time.time()
+print("It took {0} to check the size of the file".format(end-start))
+
+
